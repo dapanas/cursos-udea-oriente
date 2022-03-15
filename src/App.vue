5a0afdffccc79f477aada1863d2e25c5f0156ea3
Daniel Pérez <daniel.perez@materialworld.co>
2022-03-14 22:36:42 -0500

    Add data and render it. Remove unneeded code.

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,41 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="container-fluid">
+    <div class="row justify-content-center mt-4 mb-4">
+      <div v-for="(course, index) in courses" :key="index" class="col-auto">
+        <div class="card mt-3" style="width: 18rem;">
+          <div class="card-header text-center">
+            <img class="img-fluid mb-2"
+                 :src="'https://asone.udea.edu.co/portafolioudea/files/images/catalog-' + course.image">
+            <a :href="'http://link.udea.edu.co/portafolio?q=' + course.id" target="_blank"><h6>{{ course.name }}</h6>
+            </a>
+          </div>
+          <div class="card-body">
+            <span v-if="course.active" class="badge bg-success">Activo</span>
+            <div class="col-12">{{ course.shortPresentation }}</div>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  data() {
+    return {
+      courses: []
+    }
+  },
+  mounted() {
+    fetch("/data.json")
+        .then(response => response.json())
+        .then(data => this.courses = data.content);
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
